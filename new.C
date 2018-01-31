b6a22072333026205dd23a78e3e0efa7e7e2a3fb
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-01-31 03:10:58 -0800

    Create new.C

---
+++ new.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int 212,i;
+for (i=1;i<1;i++)
+printf("enter the bitwise number");
+{
+printf("right shift "%d,%d",&num>>i);
+print("/n");
+printf("left shift "%d,%d",&num<<i);
+}
+}
